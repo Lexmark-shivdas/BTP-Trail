14c251576db9e165d46fd7ec9ec013941ba47e11
Lexmark-shivdas <shiv.das@lexmark.com>
2022-02-26 19:37:29 +0000

    First commit

---
+++ webapp/Component.js
@@ -0,0 +1,7 @@
+sap.ui.define(["sap/suite/ui/generic/template/lib/AppComponent"], function(AppComponent) {
+    return AppComponent.extend("fioritest.Component", {
+        metadata: {
+            manifest: "json"
+        }
+    });
+});
